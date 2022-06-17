63b483ba508a51c8077c62bc09139098d3178255
Jon Balon <development@jonbalon.com>
2022-06-16 20:13:38 -0400

    Render Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
 
-import { TextField } from './TextField';
+import { Counter } from './Counter';
+
 const App: React.FC = () => {
   return <div>
-    <TextField person={{firstName: '', lastName: ''}}/>
+    <Counter>
+      {(count, setCount) => (
+      <div>
+        {count}
+
+        <button onClick={() => setCount(count + 1)}>+</button>
+      </div>)}
+    </Counter>
   </div>
 }
 
